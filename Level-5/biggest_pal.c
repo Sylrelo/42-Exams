8f7bfe9bb86ff038a5e3b3be2350a5e928d13c79
Sylvain Lopez <705H120@gmail.com>
2019-04-30 16:13:32 +0200

    -

--- Level-5/biggest_pal.c
+++ Level-5/biggest_pal.c
@@ -6,67 +6,47 @@
 /*   By: slopez <slopez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/26 23:17:08 by slopez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/26 23:24:24 by slopez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/30 16:11:47 by slopez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-typedef struct	s_pal
+typedef	struct s_pal
 {
 	int			begin;
-	int			end;
 	int			size;
 }				t_pal;
 
 void			biggest_pal(char *str)
 {
-	t_pal	pal;
+	t_pal pal;
 	int		i;
 	int		j;
-	int		t;
-	int		b;
+	int		k;
 	
-	pal = (t_pal){.begin = 0, .end = 0, .size = 0};
-	j = 0;
-	i = 0;
-	while (str[i])
+	i = -1;
+	pal = (t_pal){.begin = 0, .size = 0};
+	while (str[++i])
 	{
-		j = 1;
-		t = i;
-		b = 0;
-		while (str[t])
-		{
-			if (str[t] == str[i] && str[t + 1] == str[i])
-				b++;
-			t++;
-		}
-		while (str[i + j + b] == str[i - j])
-		{
-			if ((i + j + b) - (i - j) > pal.size)
-			{
-				pal.end = (i + j + b);
-				pal.begin = (i - j >= 0) ;
-				pal.size = pal.end - pal.begin;
-			}
-			j++;
-		}
-		i++;
+		j = 0;
+		k = 0;
+		while (str[i] == str[k + i + 1])
+			k++;
+		if (k + 1 >= pal.size)
+			pal = (t_pal) {.begin = i, .size = k + 1};
+		while (str[i + ++j + k] && str[i - j] && str[i + j + k] == str[i - j])
+			if (2 * j + k >= pal.size)
+				pal = (t_pal) {.begin = i - j, .size = 2 * j + k + 1};
 	}
-	pal.end -= pal.end < i ? 0 : 1;
-	while (pal.begin <= pal.end)
-		write(1, str + pal.begin++, 1);
-	write(1, "\n", 1);
+	write(1, str + pal.begin, pal.size);
 }
 
 int main(int ac, char *av[])
 {
-	if (ac != 2)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	biggest_pal(av[1]);
+	if (ac == 2)
+		biggest_pal(av[1]);
+	write(1, "\n", 1);
 	return (0);
 }
