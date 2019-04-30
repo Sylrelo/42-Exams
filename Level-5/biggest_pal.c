f3dbd87ad0e0b1ec3efe1f172d7805dad42a1c51
Sylvain Lopez <705H120@gmail.com>
2019-04-30 16:40:49 +0200

    -

--- Level-5/biggest_pal.c
+++ Level-5/biggest_pal.c
@@ -6,14 +6,14 @@
 /*   By: slopez <slopez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/26 23:17:08 by slopez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/30 16:30:32 by slopez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/30 16:40:40 by slopez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-typedef	struct s_pal
+typedef	struct	s_pal
 {
 	int			begin;
 	int			size;
@@ -21,11 +21,11 @@ typedef	struct s_pal
 
 void			biggest_pal(char *str)
 {
-	t_pal pal;
+	t_pal	pal;
 	int		i;
 	int		j;
 	int		k;
-	
+
 	i = -1;
 	pal = (t_pal){.begin = 0, .size = 0};
 	while (str[++i])
@@ -36,14 +36,14 @@ void			biggest_pal(char *str)
 			k++;
 		if (k >= pal.size)
 			pal = (t_pal) {.begin = i, .size = k};
-		while (++j &&  str[i + j + k - 1] && str[i - j] && str[i + j + k - 1] == str[i - j])
-			if (2 * j + k >= pal.size)
+		while (++j && str[i + j + k - 1] == str[i - j])
+			if (2 * j + k >= pal.size && str[i + j + k - 1] && str[i - j])
 				pal = (t_pal) {.begin = i - j, .size = 2 * j + k};
 	}
 	write(1, str + pal.begin, pal.size);
 }
 
-int main(int ac, char *av[])
+int				main(int ac, char *av[])
 {
 	if (ac == 2)
 		biggest_pal(av[1]);
