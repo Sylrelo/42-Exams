fca0dc3779c50cd061e007f3ca048777f49752a3
Sylvain Lopez <705H120@gmail.com>
2019-04-30 16:36:29 +0200

    -

--- Level-5/biggest_pal.c
+++ Level-5/biggest_pal.c
@@ -6,7 +6,7 @@
 /*   By: slopez <slopez@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/26 23:17:08 by slopez       #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/30 16:20:07 by slopez      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/30 16:30:32 by slopez      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -31,14 +31,14 @@ void			biggest_pal(char *str)
 	while (str[++i])
 	{
 		j = 0;
-		k = 0;
-		while (str[i] == str[k + i + 1])
+		k = 1;
+		while (str[i] == str[k + i])
 			k++;
-		if (k + 1 >= pal.size)
-			pal = (t_pal) {.begin = i, .size = k + 1};
-		while (str[i + ++j + k] && str[i - j] && str[i + j + k] == str[i - j])
-			if (2 * j + k + 1 >= pal.size)
-				pal = (t_pal) {.begin = i - j, .size = 2 * j + k + 1};
+		if (k >= pal.size)
+			pal = (t_pal) {.begin = i, .size = k};
+		while (++j &&  str[i + j + k - 1] && str[i - j] && str[i + j + k - 1] == str[i - j])
+			if (2 * j + k >= pal.size)
+				pal = (t_pal) {.begin = i - j, .size = 2 * j + k};
 	}
 	write(1, str + pal.begin, pal.size);
 }
